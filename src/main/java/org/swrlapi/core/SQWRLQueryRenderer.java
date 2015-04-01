0a0d6d1ef934a9d41ba8826117d435c678fca069
martinjoconnor <martin.oconnor@stanford.edu>
2015-04-01 15:35:01 -0700

    Separated SWRL and SQWRL renderers

---
+++ src/main/java/org/swrlapi/core/SQWRLQueryRenderer.java
@@ -0,0 +1,17 @@
+package org.swrlapi.core;
+
+import org.swrlapi.core.visitors.SWRLAPIEntityVisitorEx;
+import org.swrlapi.sqwrl.SQWRLQuery;
+
+/**
+ * A renderer for a {@link org.swrlapi.sqwrl.SQWRLQuery}.
+ *
+ * Can create using the {@link org.swrlapi.core.SWRLAPIOWLOntology}.
+ *
+ * @see org.swrlapi.sqwrl.SQWRLQuery
+ * @see org.swrlapi.core.SWRLAPIOWLOntology
+ */
+public interface SQWRLQueryRenderer extends SWRLAPIEntityVisitorEx<String>
+{
+	String renderSQWRLQuery(SQWRLQuery query);
+}
