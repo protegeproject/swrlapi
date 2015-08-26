59a16cfb7d00095625e82d9b75a321c3a3d7ab89
martin <martin@zippyrate.com>
2015-08-25 22:27:55 -0700

    Finalized test update to reflect use of prefix manager in public API

--- src/main/java/org/swrlapi/test/IntegrationTestBase.java
+++ src/main/java/org/swrlapi/test/IntegrationTestBase.java
@@ -9,26 +9,27 @@ import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import org.semanticweb.owlapi.vocab.Namespaces;
 
 import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.Datatype;
+import static org.semanticweb.owlapi.apibinding.OWLFunctionalSyntaxFactory.IRI;
 
 public class IntegrationTestBase
 {
   final protected static String NS = "http://org.swrlapi.test#";
   final protected static double DELTA = 1e-6;
 
-  protected static final OWLDatatype RDFS_LITERAL = Datatype(iri(Namespaces.RDFS + "Literal"));
-  protected static final OWLDatatype XSD_STRING = Datatype(iri(Namespaces.XSD + "string"));
-  protected static final OWLDatatype XSD_BOOLEAN = Datatype(iri(Namespaces.XSD + "boolean"));
-  protected static final OWLDatatype XSD_BYTE = Datatype(iri(Namespaces.XSD + "byte"));
-  protected static final OWLDatatype XSD_SHORT = Datatype(iri(Namespaces.XSD + "short"));
-  protected static final OWLDatatype XSD_INT = Datatype(iri(Namespaces.XSD + "int"));
-  protected static final OWLDatatype XSD_LONG = Datatype(iri(Namespaces.XSD + "long"));
-  protected static final OWLDatatype XSD_FLOAT = Datatype(iri(Namespaces.XSD + "float"));
-  protected static final OWLDatatype XSD_DOUBLE = Datatype(iri(Namespaces.XSD + "double"));
-  protected static final OWLDatatype XSD_DATE = Datatype(iri(Namespaces.XSD + "date"));
-  protected static final OWLDatatype XSD_DATETIME = Datatype(iri(Namespaces.XSD + "dateTime"));
-  protected static final OWLDatatype XSD_TIME = Datatype(iri(Namespaces.XSD + "time"));
-  protected static final OWLDatatype XSD_DURATION = Datatype(iri(Namespaces.XSD + "duration"));
-  protected static final OWLDatatype XSD_ANY_URI = Datatype(iri(Namespaces.XSD + "anyURI"));
+  protected static final OWLDatatype RDFS_LITERAL = Datatype(IRI(Namespaces.RDFS + "Literal"));
+  protected static final OWLDatatype XSD_STRING = Datatype(IRI(Namespaces.XSD + "string"));
+  protected static final OWLDatatype XSD_BOOLEAN = Datatype(IRI(Namespaces.XSD + "boolean"));
+  protected static final OWLDatatype XSD_BYTE = Datatype(IRI(Namespaces.XSD + "byte"));
+  protected static final OWLDatatype XSD_SHORT = Datatype(IRI(Namespaces.XSD + "short"));
+  protected static final OWLDatatype XSD_INT = Datatype(IRI(Namespaces.XSD + "int"));
+  protected static final OWLDatatype XSD_LONG = Datatype(IRI(Namespaces.XSD + "long"));
+  protected static final OWLDatatype XSD_FLOAT = Datatype(IRI(Namespaces.XSD + "float"));
+  protected static final OWLDatatype XSD_DOUBLE = Datatype(IRI(Namespaces.XSD + "double"));
+  protected static final OWLDatatype XSD_DATE = Datatype(IRI(Namespaces.XSD + "date"));
+  protected static final OWLDatatype XSD_DATETIME = Datatype(IRI(Namespaces.XSD + "dateTime"));
+  protected static final OWLDatatype XSD_TIME = Datatype(IRI(Namespaces.XSD + "time"));
+  protected static final OWLDatatype XSD_DURATION = Datatype(IRI(Namespaces.XSD + "duration"));
+  protected static final OWLDatatype XSD_ANY_URI = Datatype(IRI(Namespaces.XSD + "anyURI"));
 
   protected void addOWLAxioms(OWLOntology ontology, OWLAxiom... axioms)
   {
@@ -47,12 +48,13 @@ public class IntegrationTestBase
       prefixManager.copyPrefixesFrom(format.asPrefixOWLOntologyFormat().getPrefixName2PrefixMap());
 
     prefixManager.setPrefix(":", NS);
+    prefixManager.setDefaultPrefix(NS);
 
     return prefixManager;
   }
 
   protected static org.semanticweb.owlapi.model.IRI iri(String fragment)
   {
-    return org.semanticweb.owlapi.model.IRI.create(NS + "#" + fragment);
+    return org.semanticweb.owlapi.model.IRI.create(NS + fragment);
   }
 }
