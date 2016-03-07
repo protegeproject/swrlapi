d1c955b75e56c175cf1390d54d858fd2ce2cb0da
martinjoconnor <martin.oconnor@stanford.edu>
2016-03-07 11:51:21 -0800

    Clean up redundant inferred asserted OWL subclass and subproperty axioms; addressed metadatacenter/swrltab-plugin#17

--- src/main/java/org/swrlapi/factory/OWLInferredAxiomFilter.java
+++ src/main/java/org/swrlapi/factory/OWLInferredAxiomFilter.java
@@ -11,6 +11,7 @@ import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLDataPropertyDomainAxiom;
+import org.semanticweb.owlapi.model.OWLDataPropertyExpression;
 import org.semanticweb.owlapi.model.OWLDataPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLDatatypeDefinitionAxiom;
 import org.semanticweb.owlapi.model.OWLDeclarationAxiom;
@@ -32,6 +33,7 @@ import org.semanticweb.owlapi.model.OWLNegativeDataPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLNegativeObjectPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLObjectPropertyDomainAxiom;
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
 import org.semanticweb.owlapi.model.OWLObjectPropertyRangeAxiom;
 import org.semanticweb.owlapi.model.OWLReflexiveObjectPropertyAxiom;
 import org.semanticweb.owlapi.model.OWLSameIndividualAxiom;
@@ -53,11 +55,11 @@ import java.util.stream.Collectors;
  */
 public class OWLInferredAxiomFilter implements OWLAxiomVisitorEx<Boolean>
 {
-  @NonNull private final Set<@NonNull OWLAxiom> assertedAxioms;
+  @NonNull private final Set<@NonNull OWLAxiom> axioms;
 
-  public OWLInferredAxiomFilter(@NonNull Set<@NonNull OWLAxiom> assertedAxioms)
+  public OWLInferredAxiomFilter(@NonNull Set<@NonNull OWLAxiom> axioms)
   {
-    this.assertedAxioms = assertedAxioms;
+    this.axioms = axioms;
   }
 
   @NonNull @Override public Boolean visit(@NonNull OWLDeclarationAxiom owlDeclarationAxiom)
@@ -90,27 +92,41 @@ public class OWLInferredAxiomFilter implements OWLAxiomVisitorEx<Boolean>
     return true;
   }
 
+  /**
+   * Filter SCA(A, B) A is owl:Thing or where B is owl:Nothing or where there exists SCA(A, X) and SCA(X, B)
+   * where X is not owl:Thing, A, B
+   *
+   * @param axiom A subclass axiom
+   * @return True if the axiom should be filters
+   */
   @NonNull @Override public Boolean visit(@NonNull OWLSubClassOfAxiom axiom)
   {
     OWLClassExpression superClass = axiom.getSuperClass();
     OWLClassExpression subClass = axiom.getSubClass();
-    Set<OWLSubClassOfAxiom> subClassOfAxioms = this.assertedAxioms.stream().filter(OWLSubClassOfAxiom.class::isInstance)
-      .map(OWLSubClassOfAxiom.class::cast).collect(Collectors.toSet());
-    Set<OWLClassExpression> superClassSubClasses = subClassOfAxioms.stream().
-      filter(sca -> sca.getSuperClass().equals(superClass)).map(sca -> sca.getSubClass())
-      .filter(subC -> !subC.isOWLNothing() && !subC.equals(subClass)).collect(Collectors.toSet());
-    Set<OWLClassExpression> subClassSuperClasses = subClassOfAxioms.stream()
-      .filter(sca -> sca.getSubClass().equals(subClass)).map(sca -> sca.getSuperClass())
-      .filter(supC -> !supC.isOWLThing() && !supC.equals(superClass)).collect(Collectors.toSet());
-    Set<OWLClassExpression> common = new HashSet<>(superClassSubClasses);
 
-    common.retainAll(subClassSuperClasses);
+    if (superClass.equals(subClass))
+      return true;
+    else if (superClass.isOWLThing())
+      return true;
+    else if (subClass.isOWLNothing())
+      return true;
+    else {
+      Set<OWLSubClassOfAxiom> subClassOfAxioms = this.axioms.stream().filter(OWLSubClassOfAxiom.class::isInstance)
+        .map(OWLSubClassOfAxiom.class::cast).collect(Collectors.toSet());
+      Set<OWLClassExpression> superClassSubClasses = subClassOfAxioms.stream().
+        filter(sca -> sca.getSuperClass().equals(superClass)).map(sca -> sca.getSubClass())
+        .filter(subC -> !subC.isOWLNothing() && !subC.equals(subClass) && !subC.equals(superClass))
+        .collect(Collectors.toSet());
+      Set<OWLClassExpression> subClassSuperClasses = subClassOfAxioms.stream()
+        .filter(sca -> sca.getSubClass().equals(subClass)).map(sca -> sca.getSuperClass())
+        .filter(supC -> !supC.isOWLThing() && !supC.equals(superClass) && !supC.equals(subClass))
+        .collect(Collectors.toSet());
+      Set<OWLClassExpression> common = new HashSet<>(superClassSubClasses);
 
-    return !common.isEmpty();
+      common.retainAll(subClassSuperClasses);
 
-    //    return axiom.getSubClass().isOWLNothing() || axiom.getSuperClass().isOWLThing() || axiom.getSubClass()
-    //      .equals(axiom.getSuperClass());
-    //     TODO filter SCA(A, B) where there exists SCA(A, X) and SCA(X, B) where X is not owl:Thing
+      return !common.isEmpty();
+    }
   }
 
   @NonNull @Override public Boolean visit(@NonNull OWLNegativeObjectPropertyAssertionAxiom axiom)
@@ -185,10 +201,42 @@ public class OWLInferredAxiomFilter implements OWLAxiomVisitorEx<Boolean>
     return false;
   }
 
+  /**
+   * Filter SOPA(A, B) where A is owl:TopObjectProperty or where B is owl:BottomObjectProperty or there exists
+   * SOPA(A, X) and SOPA(X, B) where X is not owl:TopObjectProperty, A, B
+   *
+   * @param axiom OWL object property axiom
+   * @return True if axiom is to be filtered
+   */
   @NonNull @Override public Boolean visit(@NonNull OWLSubObjectPropertyOfAxiom axiom)
   {
-    return axiom.getSubProperty().equals(axiom.getSuperProperty()) ||
-      axiom.getSubProperty().isOWLBottomObjectProperty() || axiom.getSuperProperty().isOWLTopObjectProperty();
+    OWLObjectPropertyExpression superProperty = axiom.getSuperProperty();
+    OWLObjectPropertyExpression subProperty = axiom.getSubProperty();
+
+    if (superProperty.equals(subProperty))
+      return true;
+    else if (superProperty.isOWLTopObjectProperty())
+      return true;
+    else if (subProperty.isOWLBottomObjectProperty())
+      return true;
+    else {
+      Set<OWLSubObjectPropertyOfAxiom> subPropertyAxioms = this.axioms.stream()
+        .filter(OWLSubObjectPropertyOfAxiom.class::isInstance).map(OWLSubObjectPropertyOfAxiom.class::cast)
+        .collect(Collectors.toSet());
+      Set<OWLObjectPropertyExpression> superPropertySubProperties = subPropertyAxioms.stream().
+        filter(sopa -> sopa.getSuperProperty().equals(superProperty)).map(sopa -> sopa.getSubProperty())
+        .filter(subP -> !subP.isOWLBottomObjectProperty() && !subP.equals(subProperty) && !subP.equals(superProperty))
+        .collect(Collectors.toSet());
+      Set<OWLObjectPropertyExpression> subPropertySuperProperties = subPropertyAxioms.stream()
+        .filter(sdpa -> sdpa.getSubProperty().equals(subProperty)).map(sdpa -> sdpa.getSuperProperty())
+        .filter(supP -> !supP.isOWLTopObjectProperty() && !supP.equals(superProperty) && !supP.equals(subProperty))
+        .collect(Collectors.toSet());
+      Set<OWLObjectPropertyExpression> common = new HashSet<>(superPropertySubProperties);
+
+      common.retainAll(subPropertySuperProperties);
+
+      return !common.isEmpty();
+    }
   }
 
   @NonNull @Override public Boolean visit(@NonNull OWLDisjointUnionAxiom owlDisjointUnionAxiom)
@@ -242,10 +290,42 @@ public class OWLInferredAxiomFilter implements OWLAxiomVisitorEx<Boolean>
     return false;
   }
 
+  /**
+   * Filter SDPA(A, B) where A is owl:TopDataProperty or where B is owl:BottomDataProperty or there exists
+   * SDPA(A, X) and SDPA(X, B) where X is not owl:TopDataProperty, A, B
+   *
+   * @param axiom OWL data property axiom
+   * @return True if axiom is to be filtered
+   */
   @NonNull @Override public Boolean visit(@NonNull OWLSubDataPropertyOfAxiom axiom)
   {
-    return axiom.getSubProperty().equals(axiom.getSuperProperty()) ||
-      axiom.getSubProperty().isOWLBottomDataProperty() || axiom.getSuperProperty().isOWLTopDataProperty();
+    OWLDataPropertyExpression superProperty = axiom.getSuperProperty();
+    OWLDataPropertyExpression subProperty = axiom.getSubProperty();
+
+    if (superProperty.equals(subProperty))
+      return true;
+    else if (superProperty.isOWLTopDataProperty())
+      return true;
+    else if (subProperty.isOWLBottomDataProperty())
+      return true;
+    else {
+      Set<OWLSubDataPropertyOfAxiom> subPropertyAxioms = this.axioms.stream()
+        .filter(OWLSubDataPropertyOfAxiom.class::isInstance).map(OWLSubDataPropertyOfAxiom.class::cast)
+        .collect(Collectors.toSet());
+      Set<OWLDataPropertyExpression> superPropertySubProperties = subPropertyAxioms.stream().
+        filter(sdpa -> sdpa.getSuperProperty().equals(superProperty)).map(sdpa -> sdpa.getSubProperty())
+        .filter(subP -> !subP.isOWLBottomDataProperty() && !subP.equals(subProperty) && !subP.equals(superProperty))
+        .collect(Collectors.toSet());
+      Set<OWLDataPropertyExpression> subPropertySuperProperties = subPropertyAxioms.stream()
+        .filter(sdpa -> sdpa.getSubProperty().equals(subProperty)).map(sdpa -> sdpa.getSuperProperty())
+        .filter(supP -> !supP.isOWLTopDataProperty() && !supP.equals(superProperty) && !supP.equals(subProperty))
+        .collect(Collectors.toSet());
+      Set<OWLDataPropertyExpression> common = new HashSet<>(superPropertySubProperties);
+
+      common.retainAll(subPropertySuperProperties);
+
+      return !common.isEmpty();
+    }
   }
 
   @NonNull @Override public Boolean visit(
