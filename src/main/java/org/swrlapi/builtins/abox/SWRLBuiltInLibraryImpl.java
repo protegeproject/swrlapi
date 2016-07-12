819e66c993854946889047f8d9ce0d924bb97570
martin <martin@zippyrate.com>
2016-07-12 08:00:05 -0700

    More ABox built-in work

--- src/main/java/org/swrlapi/builtins/abox/SWRLBuiltInLibraryImpl.java
+++ src/main/java/org/swrlapi/builtins/abox/SWRLBuiltInLibraryImpl.java
@@ -12,7 +12,6 @@ import org.semanticweb.owlapi.model.parameters.Imports;
 import org.swrlapi.builtins.AbstractSWRLBuiltInLibrary;
 import org.swrlapi.builtins.arguments.SWRLBuiltInArgument;
 import org.swrlapi.builtins.arguments.SWRLMultiValueVariableBuiltInArgument;
-import org.swrlapi.builtins.arguments.SWRLNamedIndividualBuiltInArgument;
 import org.swrlapi.exceptions.SWRLBuiltInException;
 
 import java.util.HashMap;
@@ -43,23 +42,24 @@ public class SWRLBuiltInLibraryImpl extends AbstractSWRLBuiltInLibrary
 
     OWLOntology ontology = getBuiltInBridge().getOWLOntology();
     Set<OWLClassAssertionAxiom> axioms = ontology.getAxioms(AxiomType.CLASS_ASSERTION, Imports.INCLUDED);
+    boolean atLeastOneBoundArgumentUnequal = false;
 
     if (!axioms.isEmpty()) {
-      Map<Integer, OWLObject> argumentValues = new HashMap<>();
-      Map<Integer, SWRLMultiValueVariableBuiltInArgument> resultMultiArguments = new HashMap<>();
+      Map<Integer, OWLObject> boundArgumentValues = new HashMap<>();
+      Map<Integer, SWRLMultiValueVariableBuiltInArgument> unboundResultArguments = new HashMap<>();
 
       for (int argumentNumber = 0; argumentNumber < arguments.size(); argumentNumber++) {
         if (arguments.get(argumentNumber).isVariable()) {
           IRI variableIRI = arguments.get(0).asVariable().getIRI();
-          resultMultiArguments.put(argumentNumber, createSWRLMultiValueVariableBuiltInArgument(variableIRI));
+          unboundResultArguments.put(argumentNumber, createSWRLMultiValueVariableBuiltInArgument(variableIRI));
         } else {
           if (argumentNumber == 0) {
             checkThatArgumentIsAClassExpression(argumentNumber, arguments);
-            argumentValues.put(argumentNumber,
+            boundArgumentValues.put(argumentNumber,
               arguments.get(argumentNumber).asSWRLClassExpressionBuiltInArgument().getOWLClassExpression());
           } else if (argumentNumber == 1) {
             checkThatArgumentIsANamedIndividual(argumentNumber, arguments);
-            argumentValues.put(argumentNumber,
+            boundArgumentValues.put(argumentNumber,
               arguments.get(argumentNumber).asSWRLNamedIndividualBuiltInArgument().getOWLNamedIndividual());
           }
         }
@@ -69,28 +69,31 @@ public class SWRLBuiltInLibraryImpl extends AbstractSWRLBuiltInLibrary
         OWLClassExpression crav1 = axiom.getClassExpression();
         OWLNamedIndividual crav2 = axiom.getIndividual().asOWLNamedIndividual(); // We do not handle anonymous
 
-        if (argumentValues.containsKey(0) && !arguments.get(0).equals(crav1))
-          continue;
-        if (argumentValues.containsKey(1) && !arguments.get(1).equals(crav2))
-          continue;
+        atLeastOneBoundArgumentUnequal =
+          (boundArgumentValues.containsKey(0) && !arguments.get(0).equals(crav1)) || (boundArgumentValues.containsKey(1)
+            && !arguments.get(1).equals(crav2));
 
-        if (crav1.isAnonymous())
-          resultMultiArguments.get(0).addArgument(createClassExpressionBuiltInArgument(crav1));
-        else
-          resultMultiArguments.get(0).addArgument(createClassBuiltInArgument(crav1.asOWLClass()));
+        if (atLeastOneBoundArgumentUnequal)
+          break;
 
-        SWRLNamedIndividualBuiltInArgument ra2 = createNamedIndividualBuiltInArgument(crav2);
-        resultMultiArguments.get(1).addArgument(ra2);
+        if (unboundResultArguments.containsKey(0)) {
+          if (crav1.isAnonymous())
+            unboundResultArguments.get(0).addArgument(createClassExpressionBuiltInArgument(crav1));
+          else
+            unboundResultArguments.get(0).addArgument(createClassBuiltInArgument(crav1.asOWLClass()));
+        }
+
+        if (unboundResultArguments.containsKey(1))
+          unboundResultArguments.get(1).addArgument(createNamedIndividualBuiltInArgument(crav2));
       }
 
-      if (!resultMultiArguments.isEmpty()) {
-        for (Integer argumentNumber : resultMultiArguments.keySet()) {
-          if (resultMultiArguments.containsKey(argumentNumber))
-            arguments.get(argumentNumber).asVariable().setBuiltInResult(resultMultiArguments.get(argumentNumber));
-        }
-        return true;
-      } else
+      if (atLeastOneBoundArgumentUnequal)
         return false;
+      else {
+        for (Integer argumentNumber : unboundResultArguments.keySet())
+          arguments.get(argumentNumber).asVariable().setBuiltInResult(unboundResultArguments.get(argumentNumber));
+        return true;
+      }
     } else
       return false;
   }
